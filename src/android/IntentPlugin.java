03db2e649c734a57c76a7c386f8ac73331426cd0
Axel Napolitano <napolitano@users.noreply.github.com>
2016-02-05 14:00:48 +0100

    Added import

--- src/android/IntentPlugin.java
+++ src/android/IntentPlugin.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.util.Log;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.webkit.MimeTypeMap;
 
 import org.apache.cordova.CallbackContext;
